6e2d0cde0005cb56cfae963b7a992ff6859e9571
Draq <boomjay008@gmail.com>
2022-12-01 01:42:54 +0100

    feat: writing functions for handling account connection

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,12 +1,49 @@
-import React from "react";
-import Navbar from "./Navbar/Navbar";
-import { Container, Box } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
 import Footer from "./Footer/Footer";
+import Navbar from "./Navbar/Navbar";
 type Props = {
   children: React.ReactNode;
 };
 
 const Layout = ({ children }: Props) => {
+  const [ethereum, setEthereum] = useState(undefined);
+  const [connectedAccount, setConnectedAccount] = useState(undefined);
+  
+  const handleAccounts = (accounts:any) => {
+  if (accounts.length > 0) {
+    const account = accounts[0];
+    console.log('We have an authorized account: ', account);
+    setConnectedAccount(account);
+  } else {
+    console.log("No authorized accounts yet")
+  }
+};
+  const connectAccount = async () => {
+    console.log("This button was clicked")
+  if (!ethereum) {
+    alert('MetaMask is required to connect an account');
+    return;
+  }
+  // @ts-ignore
+  const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+  handleAccounts(accounts);
+  };
+  // @ts-ignore
+  useEffect(() => {
+    const getConnectedAccount = async () => {
+      if (window.ethereum) {
+        setEthereum(window.ethereum);
+      }
+    
+      if (ethereum) {
+        // @ts-ignore
+        const accounts = await ethereum.request({ method: 'eth_accounts' });
+        handleAccounts(accounts);
+      }
+    };
+    getConnectedAccount()
+  }, []);
   return (
     <Box className="pb-10">
       <Navbar />
