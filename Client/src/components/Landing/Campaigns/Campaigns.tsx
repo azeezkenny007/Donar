77477636999b67c9cb7c36be7f6c44a5fb8ac719
okhamena azeez <86110727+azeezkenny007@users.noreply.github.com>
2022-12-22 11:58:24 +0100

    added the Props needed to be passed to the campaigns

--- Client/src/components/Landing/Campaigns/Campaigns.tsx
+++ Client/src/components/Landing/Campaigns/Campaigns.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 import React from "react";
 import CampaignCard from "../../shared/Card/CampaignCards";
 import {useDonar} from '../../../DonarBackend/Donar'
+import { ContextNeeded, useFirebase } from "../../../DonarBackend/constants/Donar2";
 
 type Props = {};
 
 const Campaigns = (props: Props) => {
-
-  const { campaigns}= useDonar()
+  const {campaigns,setCampaigns} = useFirebase() as ContextNeeded
   return (
     <section className="contain my-20" id="#campaigns">
       <h1 className="text-light-text text-3xl lg:text-5xl mb-4 text-center font-semibold">
@@ -17,7 +17,7 @@ const Campaigns = (props: Props) => {
      
       <div className="flex flex-wrap justify-between gap-4 " >
      
-           <CampaignCard /> 
+           <CampaignCard  campaigns={campaigns}/> 
            <CampaignCard />
            <CampaignCard />
   
